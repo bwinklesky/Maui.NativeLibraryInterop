d5efdc6eba3a7c1d763ab9a460f5d8169caf8e80
bwinklesky <46791574+bwinklesky@users.noreply.github.com>
2026-02-06 12:31:16 -0700

    testing mapbox

---
+++ mapbox/macios/NewBinding.MaciOS.Binding/ApiDefinition.cs
@@ -0,0 +1,15 @@
+using Foundation;
+
+namespace NewBindingMaciOS
+{
+	// @interface DotnetNewBinding : NSObject
+    [Protocol]
+	[BaseType (typeof(NSObject))]
+	interface DotnetNewBinding
+	{
+		// +(NSString * _Nonnull)getStringWithMyString:(NSString * _Nonnull)myString __attribute__((warn_unused_result("")));
+		[Static]
+		[Export ("getStringWithMyString:")]
+		string GetString (string myString);
+	}
+}
