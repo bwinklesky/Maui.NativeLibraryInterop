64278b5703704b188694dce653dc89b3bed1391b
bwinklesky <46791574+bwinklesky@users.noreply.github.com>
2026-02-06 15:27:41 -0700

    adding stuff

---
+++ mapbox/macios/artifacts/MapboxCommon.xcframework/ios-arm64/MapboxCommon.framework/Headers/MBXLocationServiceObserver.h
@@ -0,0 +1,34 @@
+// This file is generated and will be overwritten automatically.
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, MBXAccuracyAuthorization);
+typedef NS_ENUM(NSInteger, MBXPermissionStatus);
+
+/** Observers for getting updates from LocationService. */
+NS_SWIFT_NAME(LocationServiceObserver)
+@protocol MBXLocationServiceObserver
+/**
+ * Called when an availability of location-based service on
+ * the device has been changed. This happens when, for example,
+ * a user disables geolocation on the device.
+ *
+ * @param isAvailable True if available, false if it's not.
+ *
+ */
+- (void)onAvailabilityChangedForIsAvailable:(BOOL)isAvailable;
+/**
+ * Called when a user has granted or revoked location permission to the app.
+ * Note that this does not work on Android because, in case of permission
+ * changes, the system will kill all running processes of the app.
+ *
+ * @param permission Updated permission of the app.
+ */
+- (void)onPermissionStatusChangedForPermission:(MBXPermissionStatus)permission;
+/**
+ * Called when a user has changed accuracy authorization for the app.
+ *
+ * @param authorization Updated accuracy authorization of the app.
+ */
+- (void)onAccuracyAuthorizationChangedForAuthorization:(MBXAccuracyAuthorization)authorization;
+@end
---
+++ mapbox/macios/artifacts/MapboxCommon.xcframework/ios-arm64/MapboxCommon.framework/Headers/MBXLogWriterBackend.h
@@ -0,0 +1,18 @@
+// This file is generated and will be overwritten automatically.
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, MBXLoggingLevel);
+
+/** Interface for implementing log writing backends, e.g. for using platform specific log backends or logging to a notification service. */
+NS_SWIFT_NAME(LogWriterBackend)
+@protocol MBXLogWriterBackend
+/**
+ * Writes a log message with a given level.
+ *
+ * @param level Severity of the message.
+ * @param message Log message
+ */
+- (void)writeLogForLevel:(MBXLoggingLevel)level
+                 message:(nonnull NSString *)message;
+@end
